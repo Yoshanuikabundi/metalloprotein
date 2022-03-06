4ead7a4df75f7df6a32e92ee1f401c859173d839
Josh Mitchell <yoshanuikabundi@gmail.com>
2022-03-06 21:47:02 +1100

    Documentation spots

--- README.md
+++ README.md
@@ -26,9 +26,9 @@ If `wasm-server-runner` is installed, `cargo run --target wasm32-unknown-unknown
 Licensed under either of
 
  * Apache License, Version 2.0
-   ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+   ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
  * MIT license
-   ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+   ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
 
 at your option.
 
--- src/elements.rs
+++ src/elements.rs
@@ -1,5 +1,11 @@
+use std::ops::Index;
+
 use crate::prelude::*;
 
+/// Materials for representations of the elements
+///
+/// May be indexed by atomic number. Index 0 is reserved for
+/// virtual sites or dummy atoms.
 pub struct ElementMaterials(pub [Handle<StandardMaterial>; 119]);
 
 impl FromWorld for ElementMaterials {
@@ -12,7 +18,19 @@ impl FromWorld for ElementMaterials {
     }
 }
 
-static ELEMENTCOLOURS: [Color; 119] = [
+impl Index<usize> for ElementMaterials {
+    type Output = Handle<StandardMaterial>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        Index::index(&self.0, index)
+    }
+}
+
+/// Colours for representations of the elements.
+///
+/// This array may be indexed by atomic number. Index 0 is reserved for virtual
+/// sites or dummy atoms.
+const ELEMENTCOLOURS: [Color; 119] = [
     Color::ANTIQUE_WHITE, // Virtual sites/dummy atoms
     Color::WHITE,         // Hydrogen,H,1
     Color::BLACK,         // Helium,He,2
@@ -134,133 +152,158 @@ static ELEMENTCOLOURS: [Color; 119] = [
     Color::BLACK,         // Oganesson,Og,118
 ];
 
-/// From Consistent van der Waals Radii for the Whole Main Group
-/// Manjeera Mantina, Adam C. Chamberlin, Rosendo Valero, Christopher J. Cramer, and Donald G. Truhlar
-/// The Journal of Physical Chemistry A 2009 113 (19), 5806-5812
-/// DOI: [10.1021/jp8111556](https://doi.org/10.1021/jp8111556)
-pub static ELEMENTRADII: [f32; 119] = [
+/// Van der Waals radii of the elements.
+///
+/// This array may be indexed by atomic number. Index 0 is reserved for virtual
+/// sites or dummy atoms and returns a value of 1.0.
+///
+/// Elements 1-60, 62-83, and 89-99 are taken from
+/// > "A cartography of the van der Waals territories"\
+/// > Santiago Alvarez\
+/// > Dalton Trans., 2013, 42, 8617-8636\
+/// > DOI: [10.1039/C3DT50599E](https://doi.org/10.1039/C3DT50599E)
+///
+/// Elements 84-88 are taken from
+/// > "Consistent van der Waals Radii for the Whole Main Group"\
+/// > Manjeera Mantina, Adam C. Chamberlin, Rosendo Valero, Christopher J. Cramer, and Donald G. Truhlar\
+/// > J. Phys. Chem. A, 2009, 113 (19), 5806-5812\
+/// > DOI: [10.1021/jp8111556](https://doi.org/10.1021/jp8111556)
+///
+/// Elements 61 and 100+ are estimated to 1 significant figure from surrounding trends.
+///
+/// A sensible alternative for elements 1-96 would be
+/// the atomic radii from
+/// > "Atomic and Ionic Radii of Elements 1-96"\
+/// > Martin Rahm, Roald Hoffman, and N. W. Ashcroft\
+/// > Chemistry - A European Journal, 2016, 22 (41), 14625-14632\
+/// > DOI: [10.1002/chem.201602949](https://doi.org/10.1002/chem.201602949)
+pub const ELEMENTRADII: [f32; 119] = [
     1.0,  // Virtual sites/dummy atoms
-    1.10, // Hydrogen,H,1
-    1.40, // Helium,He,2
-    1.81, // Lithium,Li,3
-    1.53, // Beryllium,Be,4
-    1.92, // Boron,B,5
-    1.70, // Carbon,C,6
-    1.55, // Nitrogen,N,7
-    1.52, // Oxygen,O,8
-    1.47, // Fluorine,F,9
-    1.54, // Neon,Ne,10
-    2.27, // Sodium,Na,11
-    1.73, // Magnesium,Mg,12
-    1.84, // Aluminium,Al,13
-    2.10, // Silicon,Si,14
-    1.80, // Phosphorus,P,15
-    1.80, // Sulfur,S,16
-    1.75, // Chlorine,Cl,17
-    1.88, // Argon,Ar,18
-    2.75, // Potassium,K,19
-    2.31, // Calcium,Ca,20
-    1.0,  // Scandium,Sc,21
-    1.0,  // Titanium,Ti,22
-    1.0,  // Vanadium,V,23
-    1.0,  // Chromium,Cr,24
-    1.0,  // Manganese,Mn,25
-    1.0,  // Iron,Fe,26
-    1.0,  // Cobalt,Co,27
-    1.0,  // Nickel,Ni,28
-    1.0,  // Copper,Cu,29
-    1.0,  // Zinc,Zn,30
-    1.87, // Gallium,Ga,31
-    2.11, // Germanium,Ge,32
-    1.85, // Arsenic,As,33
-    1.90, // Selenium,Se,34
-    1.83, // Bromine,Br,35
-    2.02, // Krypton,Kr,36
-    3.03, // Rubidium,Rb,37
-    2.49, // Strontium,Sr,38
-    1.0,  // Yttrium,Y,39
-    1.0,  // Zirconium,Zr,40
-    1.0,  // Niobium,Nb,41
-    1.0,  // Molybdenum,Mo,42
-    1.0,  // Technetium,Tc,43
-    1.0,  // Ruthenium,Ru,44
-    1.0,  // Rhodium,Rh,45
-    1.0,  // Palladium,Pd,46
-    1.0,  // Silver,Ag,47
-    1.0,  // Cadmium,Cd,48
-    1.93, // Indium,In,49
-    2.17, // Tin,Sn,50
-    2.06, // Antimony,Sb,51
-    2.06, // Tellurium,Te,52
-    1.98, // Iodine,I,53
-    2.16, // Xenon,Xe,54
-    3.43, // Caesium,Cs,55
-    2.68, // Barium,Ba,56
-    1.0,  // Lanthanum,La,57
-    1.0,  // Cerium,Ce,58
-    1.0,  // Praseodymium,Pr,59
-    1.0,  // Neodymium,Nd,60
-    1.0,  // Promethium,Pm,61
-    1.0,  // Samarium,Sm,62
-    1.0,  // Europium,Eu,63
-    1.0,  // Gadolinium,Gd,64
-    1.0,  // Terbium,Tb,65
-    1.0,  // Dysprosium,Dy,66
-    1.0,  // Holmium,Ho,67
-    1.0,  // Erbium,Er,68
-    1.0,  // Thulium,Tm,69
-    1.0,  // Ytterbium,Yb,70
-    1.0,  // Lutetium,Lu,71
-    1.0,  // Hafnium,Hf,72
-    1.0,  // Tantalum,Ta,73
-    1.0,  // Tungsten,W,74
-    1.0,  // Rhenium,Re,75
-    1.0,  // Osmium,Os,76
-    1.0,  // Iridium,Ir,77
-    1.0,  // Platinum,Pt,78
-    1.0,  // Gold,Au,79
-    1.0,  // Mercury,Hg,80
-    1.96, // Thallium,Tl,81
-    2.02, // Lead,Pb,82
-    2.07, // Bismuth,Bi,83
+    1.20, // Hydrogen,H,1
+    1.43, // Helium,He,2
+    2.12, // Lithium,Li,3
+    1.98, // Beryllium,Be,4
+    1.91, // Boron,B,5
+    1.77, // Carbon,C,6
+    1.66, // Nitrogen,N,7
+    1.50, // Oxygen,O,8
+    1.46, // Fluorine,F,9
+    1.58, // Neon,Ne,10
+    2.50, // Sodium,Na,11
+    2.51, // Magnesium,Mg,12
+    2.25, // Aluminium,Al,13
+    2.19, // Silicon,Si,14
+    1.90, // Phosphorus,P,15
+    1.89, // Sulfur,S,16
+    1.82, // Chlorine,Cl,17
+    1.83, // Argon,Ar,18
+    2.73, // Potassium,K,19
+    2.62, // Calcium,Ca,20
+    2.58, // Scandium,Sc,21
+    2.46, // Titanium,Ti,22
+    2.42, // Vanadium,V,23
+    2.45, // Chromium,Cr,24
+    2.45, // Manganese,Mn,25
+    2.44, // Iron,Fe,26
+    2.40, // Cobalt,Co,27
+    2.40, // Nickel,Ni,28
+    2.38, // Copper,Cu,29
+    2.39, // Zinc,Zn,30
+    2.32, // Gallium,Ga,31
+    2.29, // Germanium,Ge,32
+    1.88, // Arsenic,As,33
+    1.82, // Selenium,Se,34
+    1.86, // Bromine,Br,35
+    2.25, // Krypton,Kr,36
+    3.21, // Rubidium,Rb,37
+    2.84, // Strontium,Sr,38
+    2.75, // Yttrium,Y,39
+    2.52, // Zirconium,Zr,40
+    2.56, // Niobium,Nb,41
+    2.45, // Molybdenum,Mo,42
+    2.44, // Technetium,Tc,43
+    2.46, // Ruthenium,Ru,44
+    2.44, // Rhodium,Rh,45
+    2.15, // Palladium,Pd,46
+    2.53, // Silver,Ag,47
+    2.49, // Cadmium,Cd,48
+    2.43, // Indium,In,49
+    2.42, // Tin,Sn,50
+    2.47, // Antimony,Sb,51
+    1.99, // Tellurium,Te,52
+    2.04, // Iodine,I,53
+    2.06, // Xenon,Xe,54
+    3.48, // Caesium,Cs,55
+    3.03, // Barium,Ba,56
+    2.98, // Lanthanum,La,57
+    2.88, // Cerium,Ce,58
+    2.92, // Praseodymium,Pr,59
+    2.95, // Neodymium,Nd,60
+    3.,   // Promethium,Pm,61
+    2.90, // Samarium,Sm,62
+    2.87, // Europium,Eu,63
+    2.83, // Gadolinium,Gd,64
+    2.79, // Terbium,Tb,65
+    2.87, // Dysprosium,Dy,66
+    2.81, // Holmium,Ho,67
+    2.83, // Erbium,Er,68
+    2.79, // Thulium,Tm,69
+    2.80, // Ytterbium,Yb,70
+    2.74, // Lutetium,Lu,71
+    2.63, // Hafnium,Hf,72
+    2.53, // Tantalum,Ta,73
+    2.57, // Tungsten,W,74
+    2.49, // Rhenium,Re,75
+    2.48, // Osmium,Os,76
+    2.41, // Iridium,Ir,77
+    2.29, // Platinum,Pt,78
+    2.32, // Gold,Au,79
+    2.45, // Mercury,Hg,80
+    2.47, // Thallium,Tl,81
+    2.60, // Lead,Pb,82
+    2.54, // Bismuth,Bi,83
     1.97, // Polonium,Po,84
     2.02, // Astatine,At,85
     2.20, // Radon,Rn,86
     3.48, // Francium,Fr,87
     2.83, // Radium,Ra,88
-    1.0,  // Actinium,Ac,89
-    1.0,  // Thorium,Th,90
-    1.0,  // Protactinium,Pa,91
-    1.0,  // Uranium,U,92
-    1.0,  // Neptunium,Np,93
-    1.0,  // Plutonium,Pu,94
-    1.0,  // Americium,Am,95
-    1.0,  // Curium,Cm,96
-    1.0,  // Berkelium,Bk,97
-    1.0,  // Californium,Cf,98
-    1.0,  // Einsteinium,Es,99
-    1.0,  // Fermium,Fm,100
-    1.0,  // Mendelevium,Md,101
-    1.0,  // Nobelium,No,102
-    1.0,  // Lawrencium,Lr,103
-    1.0,  // Rutherfordium,Rf,104
-    1.0,  // Dubnium,Db,105
-    1.0,  // Seaborgium,Sg,106
-    1.0,  // Bohrium,Bh,107
-    1.0,  // Hassium,Hs,108
-    1.0,  // Meitnerium,Mt,109
-    1.0,  // Darmstadtium,Ds,110
-    1.0,  // Roentgenium,Rg,111
-    1.0,  // Copernicium,Cn,112
-    1.0,  // Nihonium,Nh,113
-    1.0,  // Flerovium,Fl,114
-    1.0,  // Moscovium,Mc,115
-    1.0,  // Livermorium,Lv,116
-    1.0,  // Tennessine,Ts,117
-    1.0,  // Oganesson,Og,118
+    2.8,  // Actinium,Ac,89
+    2.93, // Thorium,Th,90
+    2.88, // Protactinium,Pa,91
+    2.71, // Uranium,U,92
+    2.82, // Neptunium,Np,93
+    2.81, // Plutonium,Pu,94
+    2.83, // Americium,Am,95
+    3.05, // Curium,Cm,96
+    3.4,  // Berkelium,Bk,97
+    3.05, // Californium,Cf,98
+    2.7,  // Einsteinium,Es,99
+    3.,   // Fermium,Fm,100
+    3.,   // Mendelevium,Md,101
+    3.,   // Nobelium,No,102
+    3.,   // Lawrencium,Lr,103
+    3.,   // Rutherfordium,Rf,104
+    3.,   // Dubnium,Db,105
+    3.,   // Seaborgium,Sg,106
+    3.,   // Bohrium,Bh,107
+    3.,   // Hassium,Hs,108
+    3.,   // Meitnerium,Mt,109
+    3.,   // Darmstadtium,Ds,110
+    3.,   // Roentgenium,Rg,111
+    3.,   // Copernicium,Cn,112
+    3.,   // Nihonium,Nh,113
+    3.,   // Flerovium,Fl,114
+    3.,   // Moscovium,Mc,115
+    3.,   // Livermorium,Lv,116
+    3.,   // Tennessine,Ts,117
+    3.,   // Oganesson,Og,118
 ];
 
-pub static ELEMENTNAMES: [&'static str; 119] = [
+/// Names of the elements.
+///
+/// This array may be indexed by atomic number. Index 0 is reserved for virtual
+/// sites or dummy atoms and has the value "VirtualSite"
+pub const ELEMENTNAMES: [&'static str; 119] = [
     "VirtualSite",
     "Hydrogen",
     "Helium",
